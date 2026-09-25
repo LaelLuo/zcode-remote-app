ea82ff11af6e7564d5cad1f78b65f8058ca515ce
LaelLuo <laelluo@qq.com>
2026-09-25 14:46:41 +0800

    :arrow_up: chore(build): 版本 1.7.1(9)（回前台分流修正版）

--- app/app/build.gradle.kts
+++ app/app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.zcoderemote"
         minSdk = 26
         targetSdk = 34
-        versionCode = 8
-        versionName = "1.7"
+        versionCode = 9
+        versionName = "1.7.1"
     }
 
     signingConfigs {
